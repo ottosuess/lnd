5d584d6c8b5887fa36c9ca7a74279613035eb359
Johan T. Halseth <johanth@gmail.com>
2018-04-06 11:44:04 +0200

    lnrpc: add swift proto compilation

--- lnrpc/gen_protos.sh
+++ lnrpc/gen_protos.sh
@@ -5,6 +5,8 @@ protoc -I/usr/local/include -I. \
        -I$GOPATH/src \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --go_out=plugins=grpc:. \
+       --swift_out=. \
+       --swiftgrpc_out=. \
        rpc.proto
 
 
